36228b5191ca57b393f38714fa847030867d447e
Håkon Bjørhovde <hakon.bjoerhovde@gmail.com>
2022-09-12 09:29:52 +0200

    Added gitignore

--- demo_script.R
+++ demo_script.R
@@ -1,9 +1,20 @@
 # Headline
 
+<<<<<<< HEAD
 x <- rnorm(500)
 y <- rnorm(500)
+=======
+x <- rnorm(200)
+y <- rnorm(300)
+>>>>>>> crazy-idea
 
 
 # Important feature
 
+<<<<<<< HEAD
 # New feature - adding something
+=======
+# New feature
+
+# Crazy stuff
+>>>>>>> crazy-idea
